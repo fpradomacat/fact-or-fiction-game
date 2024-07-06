9d6ae03b344d39a24bfb6aa34bdbf0c1c36c630f
Federico Prado Macat <fpradomacat@gmail.com>
2024-07-06 11:38:32 +0200

    added reset() logic

--- src/app/components/players-list/players-list.component.ts
+++ src/app/components/players-list/players-list.component.ts
@@ -13,33 +13,38 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './players-list.component.scss'
 })
 export class PlayersListComponent {
+
   protected playerName: string = '';
   protected players: string[] = [];
   protected isEditing: boolean = false;
   protected editingIndex: number | null = null;
 
-  addPlayer() {
-    if (this.playerName.trim()) {
-      this.players.push(this.playerName);
-      this.playerName = '';
-    }
-  }
-
-  editPlayer(index: number) {
+  protected editPlayer(index: number): void {
     this.playerName = this.players[index];
     this.isEditing = true;
     this.editingIndex = index;
   }
 
-  savePlayer() {
+  protected savePlayer(): void {
     if (this.isEditing && this.editingIndex !== null) {
       this.players[this.editingIndex] = this.playerName;
-      this.isEditing = false;
-      this.editingIndex = null;
+      this.reset();
     } else {
       this.addPlayer();
     }
     this.playerName = '';
+  }
+
+  protected reset(): void {
+    this.isEditing = false;
+    this.editingIndex = null;
+  }
+
+  private addPlayer(): void {
+    if (this.playerName.trim()) {
+      this.players.push(this.playerName);
+      this.playerName = '';
+    }
   }
 
 }
