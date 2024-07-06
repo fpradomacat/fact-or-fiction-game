52c00276f553d0e5a78d9a96eb38a020ce1ebf59
Federico Prado Macat <fpradomacat@gmail.com>
2024-07-06 11:31:18 +0200

    players-list edition

--- src/app/components/players-list/players-list.component.ts
+++ src/app/components/players-list/players-list.component.ts
@@ -15,12 +15,31 @@ import { FormsModule } from '@angular/forms';
 export class PlayersListComponent {
   protected playerName: string = '';
   protected players: string[] = [];
+  protected isEditing: boolean = false;
+  protected editingIndex: number | null = null;
 
   addPlayer() {
     if (this.playerName.trim()) {
       this.players.push(this.playerName);
       this.playerName = '';
     }
+  }
+
+  editPlayer(index: number) {
+    this.playerName = this.players[index];
+    this.isEditing = true;
+    this.editingIndex = index;
+  }
+
+  savePlayer() {
+    if (this.isEditing && this.editingIndex !== null) {
+      this.players[this.editingIndex] = this.playerName;
+      this.isEditing = false;
+      this.editingIndex = null;
+    } else {
+      this.addPlayer();
+    }
+    this.playerName = '';
   }
 
 }
